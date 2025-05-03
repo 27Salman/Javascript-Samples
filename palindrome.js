5888937706db149e877da46af8e956cbe200edca
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-05-03 17:48:45 +0530

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,4 @@
+//palindrome
+const str="malayalam";
+const isPalindrome= str === str.split("").reverse().join("");
+console.log(isPalindrome);
---
+++ printNum.js
@@ -0,0 +1,6 @@
+//Print numbers between 1-50 that are divisible by 5.
+for(let i=1;i<=50;i++){
+    if(i%5==0){
+        console.log(i);
+    }
+}
