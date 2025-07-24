f7cfe7c5217fbc92a43546c69c138dd0875b95e2
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-07-24 10:34:16 +0530

    Add files via upload

---
+++ stringScore.js
@@ -0,0 +1,12 @@
+//Score of a String
+
+let str = "hello";
+
+var scoreOfString = function(s) {
+    let result = 0;
+    for(let i = 1; i < s.length; i++){
+        result += Math.abs(s.charCodeAt(i-1) - s.charCodeAt(i));
+    }return result;
+};
+
+console.log(scoreOfStirng(str));
