355e02e301f6158c13bb0596f099597d4335650f
SALMAN A S <133356695+27Salman@users.noreply.github.com>
2025-07-28 14:04:42 +0530

    Add files via upload

---
+++ achievableNum.js
@@ -0,0 +1,6 @@
+//Find the Maximum Achievable Number
+var theMaximumAchievableX = function(num, t) {
+    return num + (2*t);
+};
+const achieveMax = theMaximumAchievableX(4,2);
+console.log(achieveMax);
